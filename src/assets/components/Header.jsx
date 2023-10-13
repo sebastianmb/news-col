c04c452d240292bc404d213c28a19ad96b4e83c1
sebastianmb <sebastianmb88@gmail.com>
2023-10-12 22:35:11 -0500

    Se incorporan estilos a navbar para que quede como el modelo

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../images/logo.svg'
 
 export const Header = () => {
   return (
-    <header className='bg-red-400 flex place-content-between items-center'>
+    <header className='flex place-content-between items-center mb-8'>
       <img src={logo} alt="Logo"/>
       <NavBar/>
     </header>
--- src/assets/components/MainArticle.jsx
+++ src/assets/components/MainArticle.jsx
@@ -15,11 +15,11 @@ export const MainArticle = () => {
           <div className='flex-1 py-6'>
             <h2 className='text-[40px] font-bold sm:text-[58px] leading-none'> The Bright Future of Web 3.0?</h2>
           </div>
-          <div className='flex-1'>
-            <p className='text-[13px] mb-10'>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
+          <div className='flex-1 pt-9'>
+            <p className='text-[13px] mb-10 sm:text-[15px]'>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
               But is it really fulfilling its promise?
             </p>
-            <button>Read more</button>
+            <button className="bg-SoftRed w-[185px] h-[48px] uppercase text-OffWhite hover:bg-VeryDarkBlue">Read more</button>
           </div>
         </div>
       </div>
