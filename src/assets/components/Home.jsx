f3e05eb324e0fe8911afad809d4846c9487b7c2b
sebastianmb <sebastianmb88@gmail.com>
2023-10-30 23:30:36 -0500

    Retomando componente New

---
+++ src/assets/components/Home.jsx
@@ -0,0 +1,23 @@
+import { Header } from "./Header"
+import { MainArticle } from "./MainArticle"
+import { NewContainer } from "./NewContainer"
+import { ArticlesContainer } from "./ArticlesContainer"
+
+export function Home() {
+
+
+  return (
+    <main className="px-4 pt-6">
+            
+      <Header />
+      <div className="lg:flex lg:gap-8">
+        <MainArticle />
+        <NewContainer />
+
+      </div>
+      <ArticlesContainer />
+    </main>
+  )
+}
+
+
---
+++ src/assets/components/New.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./Header"
+
+
+export const New = () => {
+  return (
+    <main className="px-4 pt-6">
+            
+    <Header />
+
+  </main>
+  )
+}
+
+
