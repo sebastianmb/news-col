6074a9108e8c9165b9279620b4031bbe936745ca
sebastianmb <sebastianmb88@gmail.com>
2023-11-03 22:18:00 -0500

    Ajuste navbar

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -1,30 +1,44 @@
 import { Outlet, NavLink, Link } from "react-router-dom"
+import React, { useState } from 'react';
 import Menu from "../images/icon-menu.svg"
 
 
 export const NavBar = () => {
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
+
     return (
         <>
-        <ul  className="hidden sm:flex text-[18px] sm:w-[438px] sm:place-content-around sm:text-[16px] sm:items-center">
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <NavLink to="/new">New</NavLink>
-            </li>
-            <li>
-                <Link to="/">Popular</Link>
-            </li>
-            <li>
-                <Link to="/">Trending</Link>
-            </li>
-            <li>
-                <Link to="/">Categories</Link>
-            </li>
+            <ul className="hidden sm:flex text-[18px] sm:w-[438px] sm:place-content-around sm:text-[16px] sm:items-center">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <NavLink to="/new">New</NavLink>
+                </li>
+                <li>
+                    <Link to="/">Popular</Link>
+                </li>
+                <li>
+                    <Link to="/">Trending</Link>
+                </li>
+                <li>
+                    <button onClick={toggleMenu}>Categorias</button>
+                    {menuVisible && (
+                        <ul>
+                            <li>Opción 1</li>
+                            <li>Opción 2</li>
+                            <li>Opción 3</li>
+                        </ul>
+                    )}
+                </li>
 
-        </ul>
-        <img className='w-10 h4 cursor-pointer sm:hidden'  src={Menu} alt="Menu"/>
-        <Outlet/>
+            </ul>
+            <img className='w-10 h4 cursor-pointer sm:hidden' src={Menu} alt="Menu" />
+            <Outlet />
         </>
     )
 }
