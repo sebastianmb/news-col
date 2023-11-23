1f1158581be4d690b0f0e9138e3d3d959b9ed56e
sebastianmb <sebastianmb88@gmail.com>
2023-11-23 11:39:46 -0500

    Creación de funciones y rutas en el view crear noticia

--- backend/noticias_project/noticias_project/views.py
+++ backend/noticias_project/noticias_project/views.py
@@ -4,6 +4,26 @@ from rest_framework import generics
 from .models import Noticia, Categoria
 from .serializers import NoticiaSerializer, CategoriaSerializer
 
+
+class CrearNoticia(generics.CreateAPIView):
+    queryset = Noticia.objects.all()
+    serializer_class = NoticiaSerializer
+
+    def perform_create(self, serializer):
+        # Obtener datos adicionales del usuario o de la solicitud
+        # Ejemplo: usuario_actual = self.request.user
+        datos_solicitud = self.request.data
+
+        # Agregar lógica adicional antes de guardar la noticia
+        # Ejemplo: serializer.save(autor=usuario_actual)
+        serializer.save()  # Guardar la nueva noticia en la base de datos
+
+class DetalleNoticia(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Noticia.objects.all()
+    serializer_class = NoticiaSerializer
+
+# Otras vistas para Categoría u otras operaciones CRUD
+
 class ListaNoticias(generics.ListCreateAPIView):
     queryset = Noticia.objects.all()
     serializer_class = NoticiaSerializer
