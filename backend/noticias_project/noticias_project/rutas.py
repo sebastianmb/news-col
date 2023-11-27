f5001b927df2c18868dcd87361f1b08cf09aec26
sebastianmb <sebastianmb88@gmail.com>
2023-11-26 22:13:05 -0500

    No se pudo crear la base de datos ni el back

--- backend/noticias_project/noticias_project/rutas.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('crear-noticia/', views.CrearNoticia.as_view(), name='crear_noticia'),
-    path('noticia/<int:pk>/', views.DetalleNoticia.as_view(), name='detalle_noticia'),
-    # Otras URLs para operaciones CRUD o modelos adicionales
-]
